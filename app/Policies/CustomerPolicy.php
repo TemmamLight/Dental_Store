0ea2c5db61a3fdb941fc5fbd821b6c77f3923194
abdulrahmanRadan <aboda123li123@gmail.com>
2025-02-28 06:30:02 +0900

    implemented permission management to allow dynamic control over actions and create customer policices to safeguard customer related operations

---
+++ app/Policies/CustomerPolicy.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CustomerPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        if($user->can('view-any Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Customer $customer): bool
+    {
+        
+        if($user->can('view Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if($user->can('create Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Customer $customer): bool
+    {
+        if($user->can('update Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Customer $customer): bool
+    {
+        if($user->can('delete Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Customer $customer): bool
+    {
+        if($user->can('restore Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Customer $customer): bool
+    {
+        if($user->can('force-delete Customer')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
